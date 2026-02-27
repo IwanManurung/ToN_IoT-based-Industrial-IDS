7ba8f25440c045895e3c9f835a6344eee2092f9e
Iwan Manurung <36262977+IwanManurung@users.noreply.github.com>
2026-02-27 18:24:42 +0700

    Add files via upload

---
+++ Scenario_1_runner.py
@@ -0,0 +1,48 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+from src import ICS_IIDS_Anomaly
+import json
+import pandas as pd
+
+run_audited_version = True
+dev_under_test = 'fridge'
+class_under_test = 'normal'
+
+iids = ICS_IIDS_Anomaly(
+    anomaly_models=['Autoencoder', 'HalfSpaceTrees', 'AdaptiveIsolationForest'],
+    proba_threshold=.5,
+    normalized=True,
+    scaler_model='StandardScaler',
+    window_size=60,
+    random_seed=80,
+    run_audited_version=run_audited_version,
+    audited_size=.1)
+
+iids_layer = 'edge'
+iids.Edge_Layer_IIDS(base_device=dev_under_test,
+                        all_in_fusion=True,
+                        prediction_class=class_under_test)
+
+prefix = f'output/{iids_layer.title()}-IIDS_S1_{dev_under_test}_{class_under_test}'
+
+iids.model_output.to_csv(f'{prefix}.csv', sep=',', index=False)
+
+with open(f'{prefix}.json', 'w') as file:
+    json.dump(iids.metrics, file)
+
+del iids_layer, prefix
+
+iids_layer = 'fog'
+iids.Fog_Layer_IIDS(base_device=dev_under_test,
+                    prediction_class=class_under_test)
+    
+# save model_output and metrics
+prefix = f'output/{iids_layer.title()}-IIDS_S1_{dev_under_test}_{class_under_test}'
+
+iids.model_output.to_csv(f'{prefix}.csv', sep=',', index=False)
+
+with open(f'{prefix}.json', 'w') as file:
+    json.dump(iids.metrics, file)
+
+del iids_layer, prefix
