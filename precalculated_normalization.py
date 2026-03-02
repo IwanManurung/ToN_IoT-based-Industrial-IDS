bdaab7943c97f0e9d125656f37e12f8ba9da3f11
Iwan Manurung <manurung.iwan@gmail.com>
2026-03-03 02:24:49 +0800

    delete unwanted files

--- precalculated_normalization.py
+++
@@ -1,5 +0,0 @@
-dev = 'fridge'
-window_size = 60
-scaler = 'StandardScaler'
-fusion = True
-
--- runner.py
+++
@@ -1,3 +0,0 @@
-from _fog_fusion_procedures import data_fusion_at_fog
-fusion = data_fusion_at_fog()
-fusion.Run_at_once()
